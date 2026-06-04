b1e2d0a4a6e15825d5fe2ea7fe4256455c3a34fe
Terry.Tu <tuminfei1981@gmail.com>
2026-06-04 09:37:48 +0800

    fix event type

--- processor/src/processors/custom_event/custom_event_extractor.rs
+++ processor/src/processors/custom_event/custom_event_extractor.rs
@@ -40,7 +40,7 @@ pub const TARGET_EVENT_TYPES: &[&str] = &[
     ),
 ];
 
-pub const POWER_UPDATED_EVENT_TYPE: &str = concat!(poc_address!(), "::poc_power_store::PowerUpdatedEvent");
+pub const POWER_UPDATED_EVENT_TYPE: &str = concat!(poc_address!(), "::poc_power_store::PowerUpdateStagedEvent");
 
 pub struct CustomEventExtractor;
 
